37c66a49179533640e63a4b5ec254fe5ebc87ff2
KristianSpak <kristianspak8@gmail.com>
2020-12-30 20:31:23 +0100

    Vytvorenie Informácií Pridanie fotiek, textu a menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 const menu = document.getElementById('menu');
 
 $(document).ready(function () {
-    menu.innerHTML = "<ul><li><a href='index.html'>Domov</a></li><li><a href='#'>Hry</a><ul class='submenuGames'><li><a href='#'>Denis Hrinko</a></li><li><a href='#'>Kristián Špak</a></li><li><a href='#'>Hobo</a></li><li><a href='#'>Gošky</a></li></ul></li> <li><a href='#'>O nás</a><ul class='submenuAboutUs'><li><a href='#'>Informácie</a></li><li><a href='#'>Tabuľka</a></li></ul> </li> </ul>"
+    menu.innerHTML = "<ul><li><a href='index.html'>Domov</a></li><li><a href='#'>Hry</a><ul class='submenuGames'><li><a href='#'>Denis</a></li><li><a href='#'>Kristián</a></li><li><a href='#'>Filip</a></li><li><a href='#'>Matúš</a></li></ul></li> <li><a href='#'>O nás</a><ul class='submenuAboutUs'><li><a href='informacie.html'>Informácie</a></li><li><a href='#'>Tabuľka</a></li></ul> </li> </ul>"
 });
