8df5de608b9c5ca410031f804375083209a921ad
KristianSpak <kristianspak8@gmail.com>
2021-01-04 05:34:08 +0100

    Meniny podla mena

--- js/TEST.js
+++ js/TEST.js
@@ -2,7 +2,7 @@ let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 let yyyy = today.getFullYear();
-let i;
+let i, j;
 let name;
 window.onload = (event) =>  {
     $.getJSON("./namesday.json", function(json) {
@@ -31,22 +31,32 @@ $('#search').click(function(){
         }if(input[1].length === 1){
             input[1] = '0' + input[1][0];
         }
-        input = input[0] + input[1];
+        input = input[1] + input[0];
         $.getJSON("./namesday.json", function(json){
             for(i=0; i<json.meniny.zaznam.length;i++){
                 if(input === json.meniny.zaznam[i].den){
-                    output.innerHTML = json.meniny.zaznam[i].SK;
+                        output.innerHTML = json.meniny.zaznam[i].SK;
                 }
             }
         })
     }else{
-        console.log('weeee');
+        $.getJSON("./namesday.json", function(json){
+            for(i=0; i<json.meniny.zaznam.length;i++) {
+                if (json.meniny.zaznam[i].SK != undefined && input[0].toLowerCase() == json.meniny.zaznam[i].SK.toLowerCase()) {
+                    output.innerHTML = json.meniny.zaznam[i].den[2] + json.meniny.zaznam[i].den[3] + '.' + json.meniny.zaznam[i].den[0] + json.meniny.zaznam[i].den[1];
+                }
+            }
+        })
     }
-
 })
 
-function normalizeString(string) {
-    var input = string.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    input = input.toLowerCase();
+
+function normalizeString(input) {
+    for(i=0;i<input[0].length;i++)
+    {
+        input[0][i].normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        console.log(input)
+    }
     return input;
 }
+
