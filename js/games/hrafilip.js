726aa2c3d0039f0f7a0b4a15370a40969da14bb6
hobko <hodkyy@gmail.com>
2021-01-05 00:28:25 +0100

    celkove nahranie kus dufam ye finale pani

--- js/games/hrafilip.js
+++ js/games/hrafilip.js
@@ -1,33 +1,80 @@
-// eslint-disable-next-line import/no-unresolved
-import {Droppable} from '@shopify/draggable';
+const draggableElements = document.querySelectorAll(".mapa2");
+const droppableElements = document.querySelectorAll(".mapa");
+let i;
+let string;
+let counter = 0;
+let cas = 0;
 
-export default function UniqueDropzone() {
-    const containers = document.querySelectorAll('#UniqueDropzone .BlockLayout');
+$(document).ready(function() {
+    time();
+});
 
-    if (containers.length === 0) {
-        return false;
-    }
-
-    const droppable = new Droppable(containers, {
-        draggable: '.Block--isDraggable',
-        dropzone: '.BlockWrapper--isDropzone',
-        mirror: {
-            constrainDimensions: true,
-        },
-    });
-
-    let droppableOrigin;
-
-    // --- Draggable events --- //
-    droppable.on('drag:start', (evt) => {
-        droppableOrigin = evt.originalSource.parentNode.dataset.dropzone;
-    });
-
-    droppable.on('droppable:dropped', (evt) => {
-        if (droppableOrigin !== evt.dropzone.dataset.dropzone) {
-            evt.cancel();
-        }
-    });
-
-    return droppable;
+function time(){
+    setTimeout(function(){
+        document.getElementById('time_game').innerHTML = 'Herný čas: ' + cas + ' sek';
+        cas = cas + 1;
+        time();
+    }, 1000);
 }
+
+
+draggableElements.forEach(elem =>{
+    elem.addEventListener("dragstart", dragStart);
+});
+
+droppableElements.forEach(elem =>{
+    elem.addEventListener("dragover", dragOver);
+    elem.addEventListener("drop", drop);
+});
+
+function dragStart(event){
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function dragOver(event){
+    event.preventDefault();
+}
+
+function drop(event){
+    event.preventDefault();
+    const draggableElementData = event.dataTransfer.getData("text");
+    const droppableElementData = event.target.getAttribute("data-draggable-id");
+
+    if(draggableElementData === droppableElementData){
+
+        string = [];
+
+        for(i=0;i<draggableElementData.length-1;i++)
+        {
+            string += draggableElementData[i];
+        }
+
+        document.getElementById(string).style.opacity = '1';
+        document.getElementById(draggableElementData).style.opacity = '0';
+        counter = counter + 1;
+        if(counter === 9){
+            document.getElementById('overlay').style.display = 'none';
+            document.getElementById('win').innerHTML = 'Tvoj čas: ' + cas + 'sec';
+            document.getElementById('win').style.display = 'block';
+        }
+
+
+
+    }
+}
+
+$("#tlacidlo").click(function () {
+    draggableElements.forEach(elem =>{
+        elem.style.opacity="1";
+
+    });
+
+    droppableElements.forEach(elem =>{
+        elem.style.opacity="0.5";
+
+    });
+    cas = 0;
+    counter =0;
+    document.getElementById('overlay').style.display = "block";
+    document.getElementById('win').style.display = "none";
+})
